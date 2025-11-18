57672ad18e245b284c4c7590abaf712b70b7b104
agatherat <kulesz.agnieszka@wp.pl>
2025-11-18 22:33:53 +0100

    interface

--- VOT/Assets/Scripts/MouseLook.cs
+++ VOT/Assets/Scripts/MouseLook.cs
@@ -6,21 +6,49 @@ public class MouseLook : MonoBehaviour
     public Transform playerBody;
 
     private float xRotation = 0f;
+    private bool isCursorLocked = true;
 
-    void Start()
+      void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        LockCursor();
     }
-
+    
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        // Przełączanie trybu kursora klawiszem I
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            isCursorLocked = !isCursorLocked;
+            
+            if (isCursorLocked)
+                LockCursor();
+            else
+                UnlockCursor();
+        }
+        
+        // Obracanie kamery tylko gdy kursor jest zablokowany
+        if (isCursorLocked)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+            
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerBody.Rotate(Vector3.up * mouseX);
+        }
+    }
+    
+    void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    
+    void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
---
+++ VOT/Assets/Scripts/SelectorWindow.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectorWindow : MonoBehaviour
+{
+    [Header("UI")]
+    public GameObject panel;
+    public Dropdown dropdown;
+    public Toggle attachToggle;
+    public Toggle detachToggle;
+
+    [Header("Elementy stołu")]
+    public List<Transform> tableParts = new List<Transform>();
+    //klasa element i tam przecowywac dane takie jak is attatched/detatch,
+    // możliwosci czyli czy mozna obracac czy do goty do dolu 
+    
+    private bool isAttachMode = false;
+    void Start()
+    {
+        panel.SetActive(false);
+        UpdateDropdown();
+
+        attachToggle.onValueChanged.AddListener(OnAttachToggleChanged);
+        detachToggle.onValueChanged.AddListener(OnDetachToggleChanged);
+    }
+
+    void Update()
+{
+    if (Input.GetKeyDown(KeyCode.I))
+    {
+        panel.SetActive(!panel.activeSelf);
+        
+        // unblock coursor if panel is open
+        if (panel.activeSelf)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+}
+    void UpdateDropdown()
+    {
+        dropdown.ClearOptions();
+
+        List<string> names = new List<string>();
+
+        foreach (var part in tableParts)
+        {
+            if (part != null)
+                names.Add(part.name);
+        }
+
+        dropdown.AddOptions(names);
+
+        dropdown.onValueChanged.AddListener(OnDropdownChange);
+    }
+
+    void OnAttachToggleChanged(bool isOn)
+    {
+        if (isOn)
+        {
+            isAttachMode = true;
+            Debug.Log("element attached ");
+        }
+    }
+
+    void OnDetachToggleChanged(bool isOn)
+    {
+        if (isOn)
+        {
+            isAttachMode = false;
+            Debug.Log("element detatched");
+        }
+    }
+
+    void OnDropdownChange(int index)
+    {
+        Transform selected = tableParts[index];
+        Debug.Log("Wybrano element stołu: " + selected.name);
+        
+        if (isAttachMode)
+        {
+            AttachElement(selected);
+        }
+        else
+        {
+            DetachElement(selected);
+        }
+    }
+
+    void AttachElement(Transform element)
+    {
+        // Tu będzie kod dołączania
+        Debug.Log("DOŁĄCZAM: " + element.name);
+    }
+
+    void DetachElement(Transform element)
+    {
+        // Tu będzie kod odłączania
+        Debug.Log("ODŁĄCZAM: " + element.name);
+    }
+}
